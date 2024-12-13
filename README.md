edf38a608b2509a41d3723512a7c7943dfd5b2b9
herno <herno.russo@gmail.com>
2024-12-13 10:39:17 -0300

    Loading data from the file system
    It requires the data to be awaited before start the app.
    That is handled in the server.js file

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ The base tech stack is node + express.
 The server will run by default on port `8000`, but you can specify a different one by passing the PORT env variable i.e.: `PORT=5000`
 
 By default the API will allow cross origin request from `http://localhost:3000`
+
+# Data
+
+This app requires nasa data to be place in the root of the project in a `data` folder: `/data`
+The data format should be `csv`, and you can get it from this url: `https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=cumulative`
--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -1,3 +1,47 @@
-const planets = [];
+const { createReadStream } = require("node:fs");
+const path = require("node:path");
 
-module.exports = planets;
+const { parse } = require("csv-parse");
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (chunk) => {
+        if (isHabitablePlanet(chunk)) {
+          habitablePlanets.push(chunk);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`Found ${habitablePlanets.length} habitable planets`);
+        resolve(habitablePlanets);
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
