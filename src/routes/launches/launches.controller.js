42a2742086a478aa27972e488c5b942b6f773543
herno <herno.russo@gmail.com>
2024-12-18 10:53:40 -0300

    Add data capabilities to the models
    It turns the models into the data layer.

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -1,11 +1,9 @@
-const { launches } = require("../../models/launches.model");
+const { getAllLaunches } = require("../../models/launches.model");
 
-function getAllLaunches(req, res) {
-  console.log(launches, launches.values(), Array.from(launches.values()));
-
-  res.status(200).json(Array.from(launches.values()));
+function httpGetAllLaunches(req, res) {
+  res.status(200).json(getAllLaunches());
 }
 
 module.exports = {
-  getAllLaunches,
+  httpGetAllLaunches,
 };
